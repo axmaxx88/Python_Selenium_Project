da08150b9d2a9840512db5becc9e1d20972606b8
Максим Аксенов <ax_maxx@mail.ru>
2022-06-13 18:24:39 +0400

    Modify test_main_page file

--- test_main_page.py
+++ test_main_page.py
@@ -3,11 +3,13 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+# go to login page
 def got_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, '#login_link')
     login_link.click()
 
 
+# checking that user can go to login page
 def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     got_to_login_page(browser)
