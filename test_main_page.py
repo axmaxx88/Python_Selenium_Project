699b1471e673f6d5c9d5fc4eaf16f6d7522fd80d
Максим Аксенов <ax_maxx@mail.ru>
2022-06-19 13:52:46 +0400

    Initialize LoginPage in test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 # checking that user can go to login page
@@ -7,11 +8,13 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                     # открываем страницу
-    page.go_to_login_page()        # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()     # проверяем что на странице есть формы регистрации и авторизации
 
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_link()  # проверяем что на странице есть ссылка на страницу регистрации
