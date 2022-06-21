c32e83de03a8ca6550efaaf567909c0648617cd4
Максим Аксенов <ax_maxx@mail.ru>
2022-06-21 18:49:42 +0400

    Add 'transit from main page to basket page test'

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest
 
 
+@pytest.mark.skip
 # checking that user can go to login page
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -13,8 +16,20 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()     # проверяем что на странице есть формы регистрации и авторизации
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()  # проверяем что на странице есть ссылка на страницу регистрации
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items()
+    basket_page.should_be_the_text()
+
