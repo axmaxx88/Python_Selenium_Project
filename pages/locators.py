e32d823e9e4a4b5e9ddb9e1d4f4ba26f46c5d819
Максим Аксенов <ax_maxx@mail.ru>
2022-06-21 13:48:00 +0400

    Delete 'MainPageLocators' and create 'BasePageLocators'

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
@@ -16,3 +12,9 @@ class ProductPageLocators:
     BOOK_PRICE = (By.CSS_SELECTOR, 'p.price_color')
     BOOK_NAME_MESSAGE = (By.CSS_SELECTOR, '.alertinner strong')
     BOOK_PRICE_MESSAGE = (By.CSS_SELECTOR, '.alertinner p strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success')
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "login_link_inc")
