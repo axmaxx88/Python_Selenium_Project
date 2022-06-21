499000c26b034f0215ff84d3ac681d44d8e8b594
Максим Аксенов <ax_maxx@mail.ru>
2022-06-21 18:50:38 +0400

    Add transit from product page to basket page test

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -13,6 +15,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_message_about_price()
 
 
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -22,6 +25,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -29,6 +33,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_should_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -38,6 +43,7 @@ def test_message_should_disappeared_after_adding_product_to_basket(browser):
     page.should_disappear_success_message()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -45,8 +51,20 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_items()
+    basket_page.should_be_the_text()
+
