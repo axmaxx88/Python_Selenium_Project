456306cd5b8233c5b5caecd440392f5971f5e063
Максим Аксенов <ax_maxx@mail.ru>
2022-06-22 12:05:07 +0400

    Modify files

--- test_main_page.py
+++ test_main_page.py
@@ -5,23 +5,23 @@ from .pages.basket_page import BasketPage
 import pytest
 
 
-@pytest.mark.skip
+@pytest.mark.login_guest
 # checking that user can go to login page
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                     # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()     # проверяем что на странице есть формы регистрации и авторизации
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()                     # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()     # проверяем что на странице есть формы регистрации и авторизации
 
-
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()  # проверяем что на странице есть ссылка на страницу регистрации
+# checking that user can see login link
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()  # проверяем что на странице есть ссылка на страницу регистрации
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
